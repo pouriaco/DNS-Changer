976466045c9e280a2f26ae55eeba9d56cd32fa85
vahedi <p.pouriaco@gmail.com>
2024-05-17 14:52:34 +0330

    init app

---
+++ dns changer/dns/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace dns
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.kryptonPalette1 = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
+            this.kryptonComboBox2 = new ComponentFactory.Krypton.Toolkit.KryptonComboBox();
+            this.changeBotton = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonComboBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kryptonPalette1
+            // 
+            this.kryptonPalette1.BasePaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Global;
+            this.kryptonPalette1.ButtonSpecs.FormClose.ColorMap = System.Drawing.Color.White;
+            this.kryptonPalette1.ButtonStyles.ButtonFormClose.OverrideDefault.Back.Color1 = System.Drawing.Color.Red;
+            this.kryptonPalette1.ButtonStyles.ButtonFormClose.OverrideDefault.Back.Color2 = System.Drawing.Color.Red;
+            this.kryptonPalette1.ButtonStyles.ButtonFormClose.StateNormal.Back.Color1 = System.Drawing.Color.Red;
+            this.kryptonPalette1.ButtonStyles.ButtonFormClose.StateNormal.Back.Color2 = System.Drawing.Color.Red;
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Back.Color1 = System.Drawing.Color.White;
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Back.Color2 = System.Drawing.Color.White;
+            // 
+            // kryptonComboBox2
+            // 
+            this.kryptonComboBox2.DropDownWidth = 235;
+            this.kryptonComboBox2.Items.AddRange(new object[] {
+            "Clear",
+            "Shecan",
+            "Electro",
+            "404",
+            "Google"});
+            this.kryptonComboBox2.Location = new System.Drawing.Point(12, 12);
+            this.kryptonComboBox2.Name = "kryptonComboBox2";
+            this.kryptonComboBox2.Size = new System.Drawing.Size(251, 37);
+            this.kryptonComboBox2.StateCommon.ComboBox.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonComboBox2.StateCommon.ComboBox.Border.Rounding = 25;
+            this.kryptonComboBox2.TabIndex = 1;
+            this.kryptonComboBox2.Text = "select...";
+            this.kryptonComboBox2.SelectedIndexChanged += new System.EventHandler(this.kryptonComboBox2_SelectedIndexChanged);
+            // 
+            // changeBotton
+            // 
+            this.changeBotton.Enabled = false;
+            this.changeBotton.Location = new System.Drawing.Point(12, 60);
+            this.changeBotton.Name = "changeBotton";
+            this.changeBotton.Size = new System.Drawing.Size(251, 41);
+            this.changeBotton.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.changeBotton.StateCommon.Border.Rounding = 20;
+            this.changeBotton.StateDisabled.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.changeBotton.StateDisabled.Border.Rounding = 20;
+            this.changeBotton.StateNormal.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.changeBotton.StateNormal.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.changeBotton.StateNormal.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.changeBotton.StateNormal.Border.Rounding = 20;
+            this.changeBotton.StatePressed.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.changeBotton.StatePressed.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.changeBotton.StatePressed.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.changeBotton.StatePressed.Border.Rounding = 20;
+            this.changeBotton.StateTracking.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.changeBotton.StateTracking.Border.Rounding = 20;
+            this.changeBotton.TabIndex = 2;
+            this.changeBotton.Values.Text = "Change to selected DNS";
+            this.changeBotton.Click += new System.EventHandler(this.changeBotton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(275, 113);
+            this.Controls.Add(this.changeBotton);
+            this.Controls.Add(this.kryptonComboBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.HeaderStyle = ComponentFactory.Krypton.Toolkit.HeaderStyle.Secondary;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Palette = this.kryptonPalette1;
+            this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StateCommon.Back.Color1 = System.Drawing.Color.White;
+            this.StateCommon.Back.Color2 = System.Drawing.Color.White;
+            this.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.StateCommon.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.StateCommon.Border.Rounding = 12;
+            this.StateCommon.Header.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.StateCommon.Header.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.StateCommon.Header.Back.ColorStyle = ComponentFactory.Krypton.Toolkit.PaletteColorStyle.Dashed;
+            this.StateCommon.Header.Back.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.None;
+            this.StateCommon.Header.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.StateCommon.Header.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.StateCommon.Header.Border.ColorStyle = ComponentFactory.Krypton.Toolkit.PaletteColorStyle.Switch33;
+            this.StateCommon.Header.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.StateCommon.Header.Content.LongText.Color1 = System.Drawing.Color.White;
+            this.Tag = "";
+            this.Text = "dns changer-made by pouria";
+            this.TextExtra = "";
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonComboBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPalette kryptonPalette1;
+        private ComponentFactory.Krypton.Toolkit.KryptonComboBox kryptonComboBox2;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton changeBotton;
+    }
+}
+
---
+++ dns changer/dns/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Management;
+using System.Windows.Forms;
+
+using ComponentFactory.Krypton.Toolkit;
+
+namespace dns
+{
+    public partial class Form1 : KryptonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void kryptonComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kryptonComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            changeBotton.Enabled = true;
+        }
+
+        private void changeBotton_Click(object sender, EventArgs e)
+        {
+            switch (kryptonComboBox2.Text)
+            {
+                case "Clear":
+
+                    break;
+
+                case "Shecan":
+                    try
+                    {
+                        ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+                        ManagementObjectCollection moc = mc.GetInstances();
+                        foreach (ManagementObject mo in moc)
+                        {
+                            if ((bool)mo["IPEnabled"])
+                            {
+                                ManagementBaseObject objdns = mo.GetMethodParameters("SetDNSServerSearchOrder");
+                                if (objdns != null)
+                                {
+                                    string[] s = { "178.22.122.100", "185.51.200.2" };
+                                    objdns["DNSServerSearchOrder"] = s;
+                                    mo.InvokeMethod("SetDNSServerSearchOrder", objdns, null);
+                                }
+                            }
+                        }
+                        MessageBox.Show("! شما با موفقیت به شکن تغییر کرد DNS");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                    break;
+
+                case "Electro":
+                    try
+                    {
+                        ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+                        ManagementObjectCollection moc = mc.GetInstances();
+                        foreach (ManagementObject mo in moc)
+                        {
+                            if ((bool)mo["IPEnabled"])
+                            {
+                                ManagementBaseObject objdns = mo.GetMethodParameters("SetDNSServerSearchOrder");
+                                if (objdns != null)
+                                {
+                                    string[] s = { "78.157.42.101", "78.157.42.100" };
+                                    objdns["DNSServerSearchOrder"] = s;
+                                    mo.InvokeMethod("SetDNSServerSearchOrder", objdns, null);
+
+                                }
+                            }
+                        }
+                        MessageBox.Show("! شما با موفقیت به الکترو تغییر کرد DNS");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                    break;
+
+                case "404":
+                    try
+                    {
+                        ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+                        ManagementObjectCollection moc = mc.GetInstances();
+                        foreach (ManagementObject mo in moc)
+                        {
+                            if ((bool)mo["IPEnabled"])
+                            {
+                                ManagementBaseObject objdns = mo.GetMethodParameters("SetDNSServerSearchOrder");
+                                if (objdns != null)
+                                {
+                                    string[] s = { "10.202.10.202", "10.202.10.102" };
+                                    objdns["DNSServerSearchOrder"] = s;
+                                    mo.InvokeMethod("SetDNSServerSearchOrder", objdns, null);
+
+                                }
+                            }
+                        }
+                        MessageBox.Show("! شما با موفقیت به 404 تغییر کرد DNS");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                    break;
+
+                case "Google":
+                    try
+                    {
+                        ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+                        ManagementObjectCollection moc = mc.GetInstances();
+                        foreach (ManagementObject mo in moc)
+                        {
+                            if ((bool)mo["IPEnabled"])
+                            {
+                                ManagementBaseObject objdns = mo.GetMethodParameters("SetDNSServerSearchOrder");
+                                if (objdns != null)
+                                {
+                                    string[] s = { "8.8.8.8", "8.8.4.4" };
+                                    objdns["DNSServerSearchOrder"] = s;
+                                    mo.InvokeMethod("SetDNSServerSearchOrder", objdns, null);
+
+                                }
+                            }
+                        }
+                        MessageBox.Show("! شما با موفقیت تغییر کردDNS");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                    break;
+                default:
+                    MessageBox.Show("در حال حاضر شما دی ان اسی انتخاب نکردید");
+
+                    break;
+            }
+        }
+    }
+}
